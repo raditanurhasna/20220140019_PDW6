3ba282c3ba8a00b4b6f4a067f231a32431b4f43d
raditanurhasna <raditanurhasna1410@gmail.com>
2024-04-04 18:26:49 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click",function video(){
+    alert("hallo Swifties୭ ˚.");
+});
+
+audio.addEventListener("click",function audio(){
+    alert("hallo ୨ৎ Swifties ୨ৎ selamat mendengarkan");
+});
+
+gambar.addEventListener("click",function gambar(){
+    alert("hallo Swifties ୨ৎ");
+});
